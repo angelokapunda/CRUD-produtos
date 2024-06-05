bba35a3de5f60833e112f15824b29142863c07d9
Angelo dos Santos <angelokapunda@gmail.com>
2024-06-04 23:40:20 -0300

    feat : classe de exceptionHandler adicionada

---
+++ src/main/java/br/com/treinamento/CRUD/infra/exception/RequestExceptionHandler.java
@@ -0,0 +1,16 @@
+package br.com.treinamento.CRUD.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestExceptionHandler {
+
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404() {
+        return ResponseEntity.badRequest().body("Dodos não encontados");
+    }
+}
